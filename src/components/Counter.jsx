76086a7d0603f5d3cb16448b1c80c8336c0d7e62
Zeki <dev.zek@192.168.1.36>
2024-08-13 21:44:41 +0300

    exerciseTime:)

---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+const Counter = () => {
+  return (
+    <div className="d-flex gap-4 align-items-center">
+      <button>Azalt</button>
+      <span className="fs-1 lead">0</span>
+      <button>Arttır</button>
+    </div>
+  );
+};
+
+export default Counter;
